fc410cbca4e5d3e6f6504de22ea0d8f112c8618c
Huy Nguyen <huyngvu@gmail.com>
2016-11-04 06:34:04 +0700

    check if computed amount greater than order amount

--- app/models/spree/calculator/flat_percent_item_total_decorator.rb
+++ app/models/spree/calculator/flat_percent_item_total_decorator.rb
@@ -8,7 +8,11 @@ module Spree
 
     def compute(object, is_whole_order)
       computed_amount  = ((object.item_total + object.ship_total + object.additional_tax_total) * preferred_flat_percent / 100).round(2)
-      computed_amount
+      if computed_amount > object.amount
+        object.amount
+      else
+        computed_amount
+      end
     end
   end
 end
