438bb746321e3302031610292e7b4693cde52dc2
Huy Nguyen <huyngvu@gmail.com>
2016-11-04 05:57:30 +0700

    add missing configuration

--- lib/promotion_whole_order.rb
+++ lib/promotion_whole_order.rb
@@ -2,4 +2,10 @@ require "promotion_whole_order/version"
 
 module PromotionWholeOrder
   # Your code goes here...
+  Rails.application.config.spree.promotions.actions << Spree::Promotion::Actions::WholeOrder
+Rails.application.config.spree.calculators.add_class('promotion_actions_whole_orders')
+Rails.application.config.spree.calculators.promotion_actions_whole_orders = [
+	Spree::Calculator::FlatPercentItemTotal
+]
+
 end
